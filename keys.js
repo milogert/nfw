fee78d03c1f4f95674f52015a141945f2ead6d21
Milo Gertjejansen <milo@milogert.com>
2015-11-02 12:26:22 -0500

    Added fs require to render. Modified keys to account for separate render module.

--- keys.js
+++ keys.js
@@ -1,14 +1,22 @@
 var server = require("./server.js");
+var render = require("./render.js");
+
+var port = 9876
+if (process.argv.length == 3)
+  port = process.argv[2]
 
 // Create the server.
 server.create();
 
 // Start the server.
-server.start();
+server.start(port);
+
+console.log(server.config);
 
 // Register a cool url.
 server.register("/woo", function() {
-  return [200, {"Content-Type": "text/html"}, "found /woo mang"];
+  var sec = new Date().getTime() / 1000;
+  return [200, {"Content-Type": "text/html"}, render.render("test", {"REPLACEME": sec})];
 });
 
 server.register("/ajax", function() {
--- render.js
+++ render.js
@@ -1,3 +1,5 @@
+var fs = require("fs");
+
 // Config options.
 var config = {
   "usetemplates": false,
