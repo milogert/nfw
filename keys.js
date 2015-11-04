95764b533458285cb136acc1572755c58bd283a9
Milo Gertjejansen <milo@milogert.com>
2015-11-03 19:50:35 -0500

    Updated keys.js with uses of recent changes

--- keys.js
+++ keys.js
@@ -14,28 +14,33 @@ server.start(port, "0.0.0.0");
 console.log(server.config);
 
 // Register a cool url.
-server.register("/woo", function() {
+server.register("/render", function(cb) {
   var sec = new Date().getTime() / 1000;
-  return [200, {"Content-Type": "text/html"}, render.render("test", {"REPLACEME": sec})];
+  render.render(
+    'test',
+    { 'REPLACEME': sec },
+    function renderTest(data) {
+      cb(server.codes.success(data));
+    }); 
 });
 
 server.register("/post", function(data) {
   console.log("This is some post data: " + data["testing"]);
 }, [ "POST" ]);
 
-server.register("/ajax", function() {
-  return [200, {"Content-Type": "text/html"}, "some ajax call"];
+server.register("/ajax", function(cb) {
+  cb([200, {"Content-Type": "text/html"}, "some ajax call"]);
 });
 
-server.register("/args", function(args) {
-  return server.codes.success("This is a simple return: " + args.a + " " + args.b);
+server.register("/args", function(cb, args) {
+  cb(server.codes.success("This is a simple return: " + args.a + " " + args.b));
 });
 
-server.register("/regex/([a-z]+)/([0-9]+)", function(matches) {
-  return server.codes.success("Get some regex in the url: " + matches[1] + " " + matches[2]);
+server.register("/regex/([a-z]+)/([0-9]+)", function(cb, matches) {
+  cb(server.codes.success("Get some regex in the url: " + matches[1] + " " + matches[2]));
 });
 
-server.register("/error", function() {
-  return server.codes.error("error out.");
+server.register("/error", function(cb) {
+  cb(server.codes.error("error out."));
 });
 
